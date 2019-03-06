82a59a98a9c8a208c42b95068efa2e220b59326e
Simon Leier <simonleier@gmail.com>
2019-03-06 18:49:27 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,11 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      // color: Colors.transparent, THIS DOES NOT WORK EITHER
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
